ad16ce0d96f67037bddf7cd168772fe34a0b2b91
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-06-05 13:29:41 -0300

    improve security by sending the login token through cookies

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
+const UserValidateController = require("../controllers/UserValidateController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const userValidateController = new UserValidateController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.get('/validate', ensureAuthenticated, userValidateController.index)
 
 module.exports = usersRoutes;
